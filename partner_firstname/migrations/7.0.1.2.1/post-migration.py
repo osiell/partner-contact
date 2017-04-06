17da42b78f68491fbfe407928c4a14ce055be5da
Rémi Hauperpin-Bonneau <remi.hauperpinbonneau@live.fr>
2017-04-06 17:01:00 +0200

    [IMP] partner_firstname : improve checking and update for proper migration

--- partner_firstname/migrations/7.0.1.2.1/post-migration.py
+++ partner_firstname/migrations/7.0.1.2.1/post-migration.py
@@ -4,20 +4,25 @@
 
 
 def migrate(cr, version):
+    """Check model for proper migration."""
     cr.execute("""
-        SELECT column_name
+        SELECT count(column_name)
         FROM information_schema.columns
         WHERE table_name = 'res_partner_address'
-        AND column_name = 'first_name'
+        AND column_name
+        IN ('first_name', 'openupgrade_7_migrated_to_partner_id')
+        GROUP BY table_name
+        HAVING count(column_name) = 2
         """)
     if cr.fetchall():
         migrate_from_base_partner_surname(cr)
 
 
 def migrate_from_base_partner_surname(cr):
+    """Update res_partner from res_partner_address or former res_partner."""
     cr.execute("""
-        UPDATE res_partner
-        SET firstname = rpa.first_name, lastname = rpa.last_name
+        UPDATE res_partner rp
+        SET firstname=rpa.first_name, lastname=COALESCE(rpa.last_name, rp.name)
         FROM res_partner_address rpa
-        WHERE rpa.openupgrade_7_migrated_to_partner_id = res_partner.id
+        WHERE rpa.partner_id = rp.id
         """)
